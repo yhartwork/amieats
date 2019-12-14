c38004b9d973a52e1f3e04b71af6f985233731bc
sekarmrtata78@gmail.com <sekarmrtata78@gmail.com>
2019-12-14 17:33:21 +0700

    menambahkan kategori

--- Model/KategoriModel.cs
+++ Model/KategoriModel.cs
@@ -3,10 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace amieats.Model
 {
     class KategoriModel
     {
+        //object class Connection
+        private SqlConnection conn;
+        private SqlCommand command;
+
+        //declare variable
+        private string query;
+        private bool hasil;
+
+        //constructor 
+        public KategoriModel()
+        {
+            conn = Connection.GetConnection();
+        }
+
+        //declare attribute
+        private int id_kategori;
+        private string nama;
+
+        public int GetId_kategori()
+        {
+            return id_kategori;
+        }
+
+        public void SetId_kategori(int data)
+        {
+            id_kategori = data;
+        }
+
+        public string GetNama()
+        {
+            return nama;
+        }
+
+        public void SetNama(string data)
+        {
+            nama = data;
+        }
+
+        //fungsi menampilkan kategori
+        public DataSet SelectKategori(int id_kategori)
+        {
+            DataSet kategori = new DataSet();
+            try
+            {
+                conn.Open();
+                command = new SqlCommand();
+                command.Connection = conn;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT * FROM kategori WHERE id_kategori =" + id_kategori;
+                SqlDataAdapter sda = new SqlDataAdapter(command);
+                sda.Fill(kategori, "kategori");
+                conn.Close();
+            }
+            catch (SqlException)
+            {
+
+            }
+            return kategori;
+        }
+    
+
+            
     }
 }
--- Model/KeranjangModel.cs
+++ Model/KeranjangModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace amieats.Model
 {
     class KeranjangModel
     {
+        //object class Connection
+        private SqlConnection conn;
+        private SqlCommand command;
+
+
     }
 }
